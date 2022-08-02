42be1f3bdb1a04b21f15c7d89ab7fe01a5a8ac2f
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-08-02 11:28:24 -0400

    add constant

---
+++ aws_stepfunction/constant.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+This is based on https://states-language.net/spec.html
+"""
+
+import enum
+
+
+class StateTypeEnum(enum.Enum):
+    Task = "Task"
+    Parallel = "Parallel"
+    Map = "Map"
+    Pass = "Pass"
+    Wait = "Wait"
+    Choice = "Choice"
+    Succeed = "Succeed"
+    Fail = "Fail"
+
+
+class TopLevelFieldEnum(enum.Enum):
+    States = "States"
+    StartAt = "StartAt"
+    Comment = "Comment"
+    Version = "Version"
+    TimeoutSeconds = "TimeoutSeconds"
+
+
+class StateFieldEnum(enum.Enum):
+    Type = "Type"
+    Comment = "Comment"
+    InputPath = "InputPath"
+    OutputPath = "OutputPath"
+    Next = "Next"
+    End = "End"
+    ResultPath = "ResultPath"
+    Parameters = "Parameters"
+    ResultSelector = "ResultSelector"
+    Retry = "Retry"
+    Catch = "Catch"
