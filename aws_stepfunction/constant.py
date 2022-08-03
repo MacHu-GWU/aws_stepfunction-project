7c7cc110fcc6962edf916294f5570ef24af55d9e
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-08-02 23:13:34 -0400

    add more constant

--- aws_stepfunction/constant.py
+++ aws_stepfunction/constant.py
@@ -38,3 +38,57 @@ class StateFieldEnum(enum.Enum):
     ResultSelector = "ResultSelector"
     Retry = "Retry"
     Catch = "Catch"
+
+
+class TaskFieldEnum(enum.Enum):
+    Resource = "Resource"
+    TimeoutSecondsPath = "TimeoutSecondsPath"
+    TimeoutSeconds = "TimeoutSeconds"
+    HeartbeatSecondsPath = "HeartbeatSecondsPath"
+    HeartbeatSeconds = "HeartbeatSeconds"
+
+
+class ParallelFieldEnum(enum.Enum):
+    Branches = "Branches"
+
+
+class Enum(enum.Enum):
+    # Top level field
+    States = TopLevelFieldEnum.States.value
+    StartAt = TopLevelFieldEnum.StartAt.value
+    Comment = TopLevelFieldEnum.Comment.value
+    Version = TopLevelFieldEnum.Version.value
+    TimeoutSeconds = TopLevelFieldEnum.TimeoutSeconds.value
+
+    # State type
+    Task = StateTypeEnum.Task.value
+    Parallel = StateTypeEnum.Parallel.value
+    Map = StateTypeEnum.Map.value
+    Pass = StateTypeEnum.Pass.value
+    Wait = StateTypeEnum.Wait.value
+    Choice = StateTypeEnum.Choice.value
+    Succeed = StateTypeEnum.Succeed.value
+    Fail = StateTypeEnum.Fail.value
+
+    # State field
+    Type = StateFieldEnum.Type.value
+    # Comment = StateFieldEnum.Comment.value
+    InputPath = StateFieldEnum.InputPath.value
+    OutputPath = StateFieldEnum.OutputPath.value
+    Next = StateFieldEnum.Next.value
+    End = StateFieldEnum.End.value
+    ResultPath = StateFieldEnum.ResultPath.value
+    Parameters = StateFieldEnum.Parameters.value
+    ResultSelector = StateFieldEnum.ResultSelector.value
+    Retry = StateFieldEnum.Retry.value
+    Catch = StateFieldEnum.Catch.value
+
+    # Task state field
+    Resource = TaskFieldEnum.Resource.value
+    TimeoutSecondsPath = TaskFieldEnum.TimeoutSecondsPath.value
+    # TimeoutSeconds = TaskFieldEnum.TimeoutSeconds.value
+    HeartbeatSecondsPath = TaskFieldEnum.HeartbeatSecondsPath.value
+    HeartbeatSeconds = TaskFieldEnum.HeartbeatSeconds.value
+
+    # Parallel state field
+    Branches = ParallelFieldEnum.Branches.value
