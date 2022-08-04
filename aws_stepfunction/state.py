c9d30188805d301570e08bbc7a6197f576e2d8cc
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-08-04 15:51:20 -0400

    add attribute declaration and se order

--- aws_stepfunction/state.py
+++ aws_stepfunction/state.py
@@ -9,8 +9,9 @@ from .constant import (
     Constant as C,
     ErrorCodeEnum,
 )
-from .utils import short_uuid
+from .utils import short_uuid, is_json_path
 from .model import StepFunctionObject
+from .choice_rule import ChoiceRule
 from .state_machine import _context, StateMachine
 
 
@@ -19,38 +20,84 @@ from .state_machine import _context, StateMachine
 # ------------------------------------------------------------------------------
 @attr.s
 class State(StepFunctionObject):
+    """
+    :param _uuid: for internal implementation. we track the associated
+        state machine of each state object.
+
+    Serialization Field Order:
+
+    1. 先展示信息量最大的, 例如 Type, Comment
+    2. 再展示跟当前 State 的逻辑紧密相关的, 例如 Task 就需要关注 Resource,
+        Parallel 就需要关注 Branches, Map 就需要关注 Iterator 等等
+    3. 接下来展示跟流程相关的 Next, End
+    4. 最后展示详细的 Input Output 处理的细节
+    """
     ID: str = attr.ib(factory=lambda: short_uuid())
     Type: str = attr.ib(default=None)
     Comment: T.Optional[str] = attr.ib(default=None)
 
+    _uuid: T.Optional[str] = attr.ib(default=None)
+
     def __attrs_post_init__(self):
+        """
+        .. code-block:: python
+
+            # automatically add the task to the state machine
+            with StateMachine() as sm:
+                task = Task(...)
+        """
+        self._uuid = self.ID
         if len(_context.stack):
             sm = _context.stack[-1]
             sm.add_state(self)
 
+    @property
+    def _state_machine_id(self) -> T.Optional[str]:
+        if C.Sep in self._uuid:
+            return self._uuid.split(C.Sep, 1)[0]
+        else:
+            return None
+
     def _serialize(self) -> dict:
         data = self.to_dict()
         data.pop("ID")
+        data.pop("_uuid")
         if data.get(C.ResultPath, None) == "null":
             data[C.ResultPath] = None
         return data
 
+    # since json path attribute is so common,
+    # we should create a validator for that
+    def _check_json_path(self, attr: str, value: str):
+        if not is_json_path(value):
+            raise exc.StateValidationError.make(
+                self,
+                (
+                    f"State.{attr} = {value!r} is not a valid JSON path!"
+                )
+            )
+
+    def _check_opt_json_path(self, attr: str, value: T.Optional[str]):
+        if value is not None:
+            self._check_json_path(attr, value)
+
 
 def _create_single_machine_from_state(state: '_HasNextOrEnd') -> 'StateMachine':
     """
-    Tran
     """
-    with StateMachine() as sm:
-        sm._is_parallel_branch = True
-        sm.add_state(state)
-        sm.set_start_at(state)
+    # create a new state machine based on given state
+    with StateMachine() as new_sm:
+        new_sm._is_parallel_branch = True
+        new_sm.add_state(state)
+        new_sm.set_start_at(state)
         state.end()
 
-    top_level_sm = _context.current
-    if state.ID in top_level_sm.States:
-        top_level_sm.States.pop(state.ID)
+    # un-associate the given state from its old state machine
+    old_sm = _context.find_state_owner(state)
+    if old_sm is not None:
+        old_sm.remove_state(state)
 
-    return sm
+    return new_sm
 
 
 @attr.s
@@ -67,9 +114,6 @@ class _HasNextOrEnd(State):
             if not self.Next:
                 # when "End" is not True, you HAVE TO have "Next"
                 raise exc.StateValidationError
-
-    def _pre_serialize_validation(self):
-        self._check_next_and_end()
 
     def next(self, state: 'State') -> T.Union[
         'State',
@@ -106,6 +150,10 @@ class _HasInputOutput(State):
     InputPath: T.Optional[str] = attr.ib(default=None)
     OutputPath: T.Optional[str] = attr.ib(default=None)
 
+    def _check_input_output_path(self):
+        self._check_opt_json_path(C.InputPath, self.InputPath)
+        self._check_opt_json_path(C.OutputPath, self.OutputPath)
+
 
 @attr.s
 class _HasParameters(State):
@@ -120,6 +168,11 @@ class _HasResultSelector(State):
 @attr.s
 class _HasResultPath(State):
     ResultPath: T.Optional[str] = attr.ib(default=None)
+
+    def _check_result_path(self):
+        if self.ResultPath is not None:
+            if self.ResultPath != "null":
+                self._check_json_path(C.ResultPath, self.ResultPath)
 
     def use_task_result(self) -> T.Union[
         'Task', 'Parallel', 'Map', 'Pass'
@@ -253,6 +306,11 @@ class Task(
         C.Comment,
 
         C.Resource,
+        C.TimeoutSeconds,
+        C.TimeoutSecondsPath,
+        C.HeartbeatSeconds,
+        C.HeartbeatSecondsPath,
+
         C.Next,
         C.End,
 
@@ -264,11 +322,15 @@ class Task(
 
         C.Retry,
         C.Catch,
-        C.TimeoutSecondsPath,
-        C.TimeoutSeconds,
-        C.HeartbeatSecondsPath,
-        C.HeartbeatSeconds,
     ]
+
+    def _pre_serialize_validation(self):
+        self._check_next_and_end()
+        self._check_input_output_path()
+        self._check_result_path()
+
+        self._check_opt_json_path(C.TimeoutSecondsPath, self.TimeoutSecondsPath)
+        self._check_opt_json_path(C.HeartbeatSecondsPath, self.HeartbeatSecondsPath)
 
 
 @attr.s
@@ -289,6 +351,7 @@ class Parallel(
         C.Comment,
 
         C.Branches,
+
         C.Next,
         C.End,
 
@@ -310,12 +373,13 @@ class Parallel(
 
     def _pre_serialize_validation(self):
         self._check_next_and_end()
+        self._check_input_output_path()
+        self._check_result_path()
+
         self._check_branches()
 
-
     def _serialize(self) -> dict:
-        data = self.to_dict()
-        data.pop("ID")
+        data = super()._serialize()
 
         branches = list()
         for state_machine in self.Branches:
@@ -334,11 +398,20 @@ class Map(
     _HasResultPath,
     _HasRetryCatch,
 ):
+    ID: str = attr.ib(factory=lambda: f"{C.Map}-{short_uuid()}")
+    Type: str = attr.ib(default=C.Map)
+    Iterator: T.Optional['StateMachine'] = attr.ib(default=None)
+    ItemsPath: T.Optional['str'] = attr.ib(default=None)
+    MaxConcurrency: T.Optional[int] = attr.ib(default=None)
+
     _key_order = [
         C.Type,
         C.Comment,
 
-        C.Branches,
+        C.Iterator,
+        C.ItemsPath,
+        C.MaxConcurrency,
+
         C.Next,
         C.End,
 
@@ -351,6 +424,13 @@ class Map(
         C.Retry,
         C.Catch,
     ]
+
+    def _pre_serialize_validation(self):
+        self._check_next_and_end()
+        self._check_input_output_path()
+        self._check_result_path()
+
+        self._check_opt_json_path(C.ItemsPath, self.ItemsPath)
 
 
 class Pass(
@@ -375,17 +455,37 @@ class Pass(
         C.OutputPath,
     ]
 
+    def _pre_serialize_validation(self):
+        self._check_next_and_end()
+        self._check_input_output_path()
+        self._check_result_path()
+
 
 class Wait(
     _HasInputOutput,
     _HasNextOrEnd,
 ):
+    """
+    Reference:
+
+    - https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-wait-state.html
+    """
     ID: str = attr.ib(factory=lambda: f"{C.Wait}-{short_uuid()}")
     Type: str = attr.ib(default=C.Wait)
+
+    Seconds: T.Optional[int] = attr.ib(default=None)
+    Timestamp: T.Optional[str] = attr.ib(default=None)
+    SecondsPath: T.Optional[str] = attr.ib(default=None)
+    TimestampPath: T.Optional[str] = attr.ib(default=None)
 
     _key_order = [
         C.Type,
         C.Comment,
+
+        C.Seconds,
+        C.Timestamp,
+        C.SecondsPath,
+        C.TimestampPath,
 
         C.Next,
         C.End,
@@ -394,30 +494,43 @@ class Wait(
         C.OutputPath,
     ]
 
-
-@attr.s
-class Option:
-    pass
+    def _pre_serialize_validation(self):
+        self._check_next_and_end()
+        self._check_input_output_path()
 
 
 class Choice(
     _HasInputOutput
 ):
+    """
+    Reference:
+
+    - https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-choice-state.html
+    """
     ID: str = attr.ib(factory=lambda: f"{C.Choice}-{short_uuid()}")
     Type: str = attr.ib(default=C.Choice)
-    Choices: T.List['Option'] = attr.ib(factory=list)
+    Choices: T.List['ChoiceRule'] = attr.ib(factory=list)
+    Default: T.Optional[str] = attr.ib(default=None)
 
     _key_order = [
         C.Type,
         C.Comment,
 
+        C.Choices,
+        C.Default,
+
         C.InputPath,
         C.OutputPath,
     ]
 
-    def _pre_serialize_validation(self):
+    def _check_choices(self):
         if len(self.Choices) == 0:
-            raise ValidationError
+            raise exc.StateValidationError
+
+    def _pre_serialize_validation(self):
+        self._check_input_output_path()
+
+        self._check_choices()
 
 
 class Succeed(
@@ -434,14 +547,22 @@ class Succeed(
         C.OutputPath,
     ]
 
+    def _pre_serialize_validation(self):
+        self._check_input_output_path()
+
 
 class Fail(
     State,
 ):
     ID: str = attr.ib(factory=lambda: f"{C.Fail}-{short_uuid()}")
     Type: str = attr.ib(default=C.Fail)
+    Cause: T.Optional[str] = attr.ib(default=None)
+    Error: T.Optional[str] = attr.ib(default=None)
 
     _key_order = [
         C.Type,
         C.Comment,
+
+        C.Cause,
+        C.Error,
     ]
