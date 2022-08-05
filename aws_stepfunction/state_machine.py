a8641e208b3d438044e2edb59c52476fc0c19d72
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-08-04 22:53:45 -0400

    change the way how statemachine works now it is a workflow

--- aws_stepfunction/state_machine.py
+++ aws_stepfunction/state_machine.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import typing as T
-from collections import OrderedDict
+from collections import OrderedDict, deque
 
 import attr
 
@@ -11,22 +11,35 @@ from .model import StepFunctionObject
 from . import exc
 
 if T.TYPE_CHECKING:
-    from .state import State
+    from .state import StateType
 
 
 @attr.s
 class Context:
+    """
+    :param stack: track the current top level state machine
+    :param state_machines: all declared state machine metadata
+    """
     stack: T.List['StateMachine'] = attr.ib(factory=list)
+    state_machines: T.Dict[str, 'StateMachine'] = attr.ib(factory=dict)
 
     def push(self, sm: 'StateMachine'):
         self.stack.append(sm)
+        self.state_machines[sm.ID] = sm
 
     def pop(self) -> 'StateMachine':
-        return self.stack.pop()
+        sm = self.stack.pop()
+        return sm
 
     @property
     def current(self) -> 'StateMachine':
         return self.stack[-1]
+
+    def find_state_owner(self, state: 'State') -> T.Optional['StateMachine']:
+        if state._state_machine_id is None:
+            return None
+        else:
+            return self.state_machines[state._state_machine_id]
 
 
 _context = Context()
@@ -39,14 +52,17 @@ __a_1_state_machine = None
 
 @attr.s
 class StateMachine(StepFunctionObject):
-    ID: str = attr.ib(factory=lambda: f"StateMachine-{short_uuid()}")
-    StartAt: str = attr.ib(default="")
-    Comment: T.Optional[str] = attr.ib(default=None)
-    States: T.OrderedDict[str, 'State'] = attr.ib(factory=OrderedDict)
-    Version: T.Optional[str] = attr.ib(default=None)
-    TimeoutSeconds: T.Optional[int] = attr.ib(default=None)
+    id: str = attr.ib(factory=lambda: f"StateMachine-{short_uuid()}")
+    start_at: str = attr.ib(default="")
+    comment: T.Optional[str] = attr.ib(default=None)
+    states: T.OrderedDict[str, 'StateType'] = attr.ib(factory=OrderedDict)
+    version: T.Optional[str] = attr.ib(default=None)
+    timeout_seconds: T.Optional[int] = attr.ib(default=None)
+
+    _previous_state: T.Optional['StateType'] = attr.ib(default=None)
 
     _is_parallel_branch: bool = attr.ib(default=False)
+    _state_orders: T.Deque[str] = attr.ib(factory=deque)
 
     def __enter__(self) -> 'StateMachine':
         _context.push(self)
@@ -55,47 +71,127 @@ class StateMachine(StepFunctionObject):
     def __exit__(self, exc_type, exc_value, exc_traceback):
         _context.pop()
 
-    def add_state(self, state: 'State'):
-        if state.ID in self.States:
+    def add_state(self, state: 'StateType'):
+        if state.id in self.states:
             raise exc.StateMachineError.make(
                 self,
-                f"Cannot add State(ID={state.ID!r}), "
+                f"Cannot add State(ID={state.id!r}), "
                 f"It is already defined!"
             )
         else:
-            self.States[state.ID] = state
+            self.states[state.id] = state
+            # state._uuid = f"{self.ID}{C.Sep}{state.id}"
 
-    def set_start_at(self, state: 'State'):
-        self.StartAt = state.ID
+    def remove_state(self, state: 'StateType'):
+        if state.id not in self.states:
+            raise exc.StateMachineError.make(
+                self, ""
+            )
+        else:
+            self.states.pop(state.id)
+            state._state_machine = {state.id}
+
+    def set_start_at(self, state: 'StateType'):
+        self.StartAt = state.id
+
+    # Workflow
+    def start(self, state: 'StateType'):
+        self.start_at = state.id
+        self.add_state(state)
+        self._previous_state = state
+        return self
+
+    def next(self, state: 'StateType'):
+        self._previous_state.next = state.id
+        self.add_state(state)
+        self._previous_state = state
+        return self
+
+    #
+    # def parallel(
+    #     self,
+    #     branches: T.Iterable[T.Union['StateType', 'StateMachine']],
+    #     id: T.Optional[str] = None,
+    # ) -> 'Parallel':
+    #     state_machine_list: T.List[StateMachine] = list()
+    #     for item in branches:
+    #         if isinstance(item, _HasNextOrEnd):
+    #             state_machine = _create_single_machine_from_state(item)
+    #             state_machine_list.append(state_machine)
+    #         elif isinstance(item, StateMachine):
+    #             state_machine_list.append(item)
+    #         else:
+    #             raise TypeError
+    #
+    #     kwargs = {C.Branches: state_machine_list}
+    #     if id is not None:
+    #         kwargs[C.ID] = id
+    #
+    #     para = Parallel(**kwargs)
+    #     self.next(para)
+    #     return para
+    #
+    # def map(
+    #     self,
+    #     iterator: T.Union['StateType', 'StateMachine'],
+    #     items_path: T.Optional[str] = None,
+    # ) -> 'Map':
+    #     pass
+    #
+    def choice(
+        self,
+        choices: T.Iterable[T.Union['StateType', 'StateMachine']],
+        default: T.Optional['StateType'] = None,
+    ):
+        return self
+    #
+    # def wait(self, seconds: int) -> 'Wait':
+    #     pass
+    #
+    # def passing(self) -> 'Pass':
+    #     pass
+    #
+    # def succeed(self) -> 'Succeed':
+    #     pass
+    #
+    # def fail(self) -> 'Fail':
+    #     pass
+    #
+    def end(self):
+        self._previous_state.end = True
+        return self
+
+    def continue_from(self, state: 'StateType') -> 'StateType':
+        return state
 
     def _pre_serialize_validation(self):
-        if not self.StartAt:
+        if not self.start_at:
             raise exc.StateMachineValidationError.make(
                 self,
                 f"'StartAt' cannot be empty string!"
             )
-        if self.StartAt not in self.States:
+        if self.start_at not in self.states:
             raise exc.StateMachineValidationError(
                 self,
-                f"'StartAt' id {self.StartAt!r} is not any of defined State ID"
+                f"'StartAt' id {self.start_at!r} is not any of defined State ID"
             )
 
     def _serialize(self) -> dict:
         data = {
-            C.StartAt: self.StartAt,
+            C.StartAt: self.start_at,
             C.States: {
-                state_id: state._serialize()
-                for state_id, state in self.States.items()
+                state_id: state.serialize()
+                for state_id, state in self.states.items()
             },
         }
         if self._is_parallel_branch is True:
             return data
 
-        if self.Comment:
-            data[C.Comment] = self.Comment
-        if self.Version:
-            data[C.Version] = self.Version
-        if self.TimeoutSeconds:
-            data[C.TimeoutSeconds] = self.TimeoutSeconds
+        if self.comment:
+            data[C.Comment] = self.comment
+        if self.version:
+            data[C.Version] = self.version
+        if self.timeout_seconds:
+            data[C.TimeoutSeconds] = self.timeout_seconds
 
         return data
--- tests/test_state_machine.py
+++ tests/test_state_machine.py
@@ -2,53 +2,128 @@
 
 import os
 import pytest
+
+from rich import print as rprint
+
 from aws_stepfunction import exc
 from aws_stepfunction.state_machine import _context, StateMachine
-
-
-class TestContext:
-    def test(self):
-        # at begin we got nothing in context queue
-        assert len(_context.stack) == 0
-        with StateMachine(
-            ID="sm1",
-            Comment="First State Machine",
-        ) as sm1:
-            assert len(_context.stack) == 1
-            assert _context.current.ID == sm1.ID
-
-            with StateMachine(
-                ID="sm2",
-                Comment="Second State Machine",
-            ) as sm2:
-                assert len(_context.stack) == 2
-                assert _context.current.ID == sm2.ID
-
-            assert len(_context.stack) == 1
-            assert _context.current.ID == sm1.ID
-
-            with StateMachine(
-                ID="sm3",
-                Comment="Third State Machine",
-            ) as sm3:
-                assert len(_context.stack) == 2
-                assert _context.current.ID == sm3.ID
-
-            assert len(_context.stack) == 1
-            assert _context.current.ID == sm1.ID
-
-        assert len(_context.stack) == 0
+from aws_stepfunction.state import Task
+from aws_stepfunction.choice_rule import Var
 
 
 class TestStateMachine:
-    def test_pre_serialize_validations(self):
-        with pytest.raises(exc.StateMachineValidationError):
-            sm = StateMachine()
-            sm.serialize()
+    def test_workflow(self):
+        sm = StateMachine()
 
-        with pytest.raises(exc.StateMachineValidationError):
-            sm = StateMachine(StartAt="void")
-            sm.serialize()
+        t01 = Task(id="T01", resource="arn")
+        t02 = Task(id="T02", resource="arn")
+        t03_a1 = Task(id="T03-a1", resource="arn")
+        t03_a2 = Task(id="T03-a2", resource="arn")
+        t03_b1 = Task(id="T03-b1", resource="arn")
+        t03_b2 = Task(id="T03-b2", resource="arn")
+        t03_c = Task(id="T03-c", resource="arn")
+        t04 = Task(id="T04", resource="arn")
+        t05_a1 = Task(id="T05-a1", resource="arn")
+        t05_a2 = Task(id="T05-a2", resource="arn")
+        t05_b1 = Task(id="T05-b1", resource="arn")
+        t05_b2 = Task(id="T05-b2", resource="arn")
+        t06 = Task(id="T06", resource="arn")
+        t07 = Task(id="T07", resource="arn")
+        t08 = Task(id="T08", resource="arn")
+        t09 = Task(id="T09", resource="arn")
+        t10 = Task(id="T10", resource="arn")
+
+        (
+            sm.start(t01)
+            .next(t02)
+            .choice([
+                (
+                    Var("$.key").string_equals("v1")
+                    # .next(t03_a1)
+                    # .next(t03_a2)
+                    # .next(t04)
+                ),
+                # (
+                #     Var("$.key").string_equals("v2")
+                #     .next(t03_b1)
+                #     .next(t03_b2)
+                #     .next(t04)
+                # ),
+                # (
+                #     Var("$.key").string_equals("v2")
+                #     .next(t03_b1)
+                #     .next(t03_b2)
+                #     .next(t04)
+                # )
+            ])
+            # .default_fail()
+            .end()
+        )
+        #
+        # (
+        #     sm.continue_from(t04)
+        #     .parallel([
+        #         t05_a1.next(t05_a2).end(),
+        #         t05_b1.next(t05_b2).end(),
+        #     ])
+        #     .next(t06)
+        #     .wait(
+        #         seconds=10,
+        #     )
+        #     .next(t07)
+        #     .map(
+        #         t08.next(t09),
+        #         items_path="$.items",
+        #     )
+        #     .next(t10)
+        #     .end()
+        # )
+
+        rprint(sm.serialize())
+
+# class TestContext:
+#     def test(self):
+#         # at begin we got nothing in context queue
+#         assert len(_context.stack) == 0
+#         with StateMachine(
+#             ID="sm1",
+#             Comment="First State Machine",
+#         ) as sm1:
+#             assert len(_context.stack) == 1
+#             assert _context.current.ID == sm1.ID
+#
+#             with StateMachine(
+#                 ID="sm2",
+#                 Comment="Second State Machine",
+#             ) as sm2:
+#                 assert len(_context.stack) == 2
+#                 assert _context.current.ID == sm2.ID
+#
+#             assert len(_context.stack) == 1
+#             assert _context.current.ID == sm1.ID
+#
+#             with StateMachine(
+#                 ID="sm3",
+#                 Comment="Third State Machine",
+#             ) as sm3:
+#                 assert len(_context.stack) == 2
+#                 assert _context.current.ID == sm3.ID
+#
+#             assert len(_context.stack) == 1
+#             assert _context.current.ID == sm1.ID
+#
+#         assert len(_context.stack) == 0
+#
+#
+# class TestStateMachine:
+#     def test_pre_serialize_validations(self):
+#         with pytest.raises(exc.StateMachineValidationError):
+#             sm = StateMachine()
+#             sm.serialize()
+#
+#         with pytest.raises(exc.StateMachineValidationError):
+#             sm = StateMachine(StartAt="void")
+#             sm.serialize()
 
 
 if __name__ == "__main__":
