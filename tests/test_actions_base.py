93028ba05a3bb37e38070c88186f8733049b44cd
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-08-06 21:29:39 -0400

    fix unit test

--- tests/test_actions_base.py
+++ tests/test_actions_base.py
@@ -9,13 +9,9 @@ from aws_stepfunction.actions.base import (
 )
 
 
-@pytest.fixture
-def set_task_context():
-    task_context.aws_account_id = "111122223333"
-    task_context.aws_region = "us-east-1"
-
-
 def test_resolve_resource_arn():
+    task_context.reset()
+
     with pytest.raises(ValueError):
         _resolve_resource_arn(resource_name="hello", resource_type="lambda", path="")
 
