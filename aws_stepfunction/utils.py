a1899d4189ad17ed0b000f874c2cbc381a527025
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-11-08 16:15:10 -0500

    add naming convention converter utilities

--- aws_stepfunction/utils.py
+++ aws_stepfunction/utils.py
@@ -4,6 +4,7 @@
 Utility functions.
 """
 
+import typing as T
 import uuid
 import hashlib
 
@@ -22,3 +23,36 @@ def is_json_path(path: str) -> bool:
     Verify if string is a valid JSON path.
     """
     return path.startswith("$")
+
+
+DELIMITERS = ["_", "-", " "]
+
+
+def tokenize(s: str) -> T.List[str]:
+    """
+    Tokenize the text into words.
+    """
+    for sep in DELIMITERS:
+        s = s.replace(sep, " ")
+    return [token for token in s.split(" ") if token]
+
+
+def slugify(s: str) -> str:
+    """
+    Convert to lowercase + hyphen naming convention.
+    """
+    return "-".join([token.lower() for token in tokenize(s)])
+
+
+def snake_case(s: str) -> str:
+    """
+    Convert to lowercase + underscore naming convention.
+    """
+    return "_".join([token.lower() for token in tokenize(s)])
+
+
+def camel_case(s: str) -> str:
+    """
+    Convert to first letter uppercase + no space naming convention.
+    """
+    return "".join([token[0].upper() + token[1:].lower() for token in tokenize(s)])
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,6 +15,22 @@ def test_is_json_path():
     assert utils.is_json_path("abc") is False
 
 
+def test_tokenize():
+    assert utils.tokenize(" h e_l-l o    world") == ["h", "e", "l", "l", "o", "world"]
+
+
+def test_slugify():
+    assert utils.slugify("  Hello_World  ") == "hello-world"
+
+
+def test_snake_case():
+    assert utils.snake_case("  Hello-World  ") == "hello_world"
+
+
+def test_camel_case():
+    assert utils.camel_case("  hello_world  ") == "HelloWorld"
+
+
 if __name__ == "__main__":
     basename = os.path.basename(__file__)
     pytest.main([basename, "-s", "--tb=native"])
